8c2617c6f1a0e3c6c055654ca51cf48b5070ed54
Uriah Finkel <ufinkel@gmail.com>
2025-11-28 13:44:44 +0200

    build: initial commit

---
+++ eda.py
@@ -0,0 +1,161 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "marimo>=0.17.0",
+#     "pyzmq",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.18.1"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import polars as pl
+
+    raw_data = pl.read_csv("data\creditcard.csv",
+        schema_overrides={"Time": pl.Float64})
+    return pl, raw_data
+
+
+@app.cell
+def _(raw_data):
+    raw_data
+    return
+
+
+@app.cell
+def _(raw_data):
+    import numpy as np
+
+    # Set a random seed for reproducibility
+    np.random.seed(42)
+
+    # Shuffle the data
+    raw_data_sample = raw_data.sample(n=raw_data.height, with_replacement=False)
+
+    # Determine the sizes of each set
+    total_rows = raw_data_sample.height
+    train_size = int(total_rows * 0.7)  # 70% for training
+    validation_size = int(total_rows * 0.15)  # 15% for validation
+    test_size = total_rows - train_size - validation_size  # remaining 15% for testing
+
+    # Split the data
+    train_data = raw_data_sample[:train_size]
+    validation_data = raw_data_sample[train_size:train_size + validation_size]
+    test_data = raw_data_sample[train_size + validation_size:]
+    return (train_data,)
+
+
+@app.cell
+def _(train_data):
+    train_data.describe()
+    return
+
+
+@app.cell
+def _(train_data):
+    train_data
+    return
+
+
+@app.cell
+def _(train_data):
+
+    X = train_data.drop("Class").to_numpy()
+    y = train_data["Class"].to_numpy()
+    return X, y
+
+
+@app.cell
+def _():
+    from sklearn.linear_model import LogisticRegression
+
+    clf = LogisticRegression(
+        max_iter=1000,
+        solver="liblinear",   # works well for binary, sparse-ish data
+        class_weight="balanced"  # handles the heavy class imbalance
+    )
+    return (clf,)
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _(X, clf, y):
+    clf.fit(X, y)
+    return
+
+
+@app.cell
+def _(X, clf):
+    preds = clf.predict_proba(X)[:,1]
+
+    preds
+    return (preds,)
+
+
+@app.cell
+def _(preds, y):
+    from rtichoke.performance_data.performance_data import prepare_performance_data
+
+    prepare_performance_data(
+        probs = {"model": preds},
+        reals = {"model": y},
+        by=0.1
+    )
+    return
+
+
+@app.cell
+def _(preds, y):
+    from rtichoke.discrimination.roc import create_roc_curve
+
+    create_roc_curve(
+        probs = {"model": preds},
+        reals = {"model": y},
+        by=0.1
+    )
+    return
+
+
+@app.cell
+def _(y):
+    y
+    return
+
+
+@app.cell
+def _(preds, y):
+    from rtichoke.discrimination.precision_recall import create_precision_recall_curve
+
+    create_precision_recall_curve(
+        probs = {"model": preds},
+        reals = {"model": y},
+        by=0.01
+    )
+    return
+
+
+@app.cell
+def _(pl):
+    pl.__version__
+    return
+
+
+@app.cell
+def _():
+    import rtichoke
+
+    rtichoke.__version__
+    return
+
+
+if __name__ == "__main__":
+    app.run()
